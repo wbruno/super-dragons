4e5adc109d0ec548e160dc57077ee0094eb82bcf
William Bruno <rocha_bruno@hotmail.com>
2015-06-06 17:28:05 -0300

    broadcast message

--- src/js/game.js
+++ src/js/game.js
@@ -5,3 +5,16 @@ var socket = io.connect(SOCKET_SERVER);
 socket.on('turn', function(msg){
   console.log(msg);
 });
+
+var $cardInfoItems = document.querySelectorAll('.card-info-item');
+$cardInfoItems = [].slice.call($cardInfoItems);
+
+$cardInfoItems.forEach(function($item) {
+  $item.addEventListener('click', onItemClick);
+});
+
+function onItemClick(event) {
+  var value = this.getAttribute('data-info');
+
+  socket.emit('turn', value);
+}
